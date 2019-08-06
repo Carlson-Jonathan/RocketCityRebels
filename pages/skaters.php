e2fb4feccc1ce24e8b0476c20c06cb43845ff869
Jonathan Carlson <Car99020@byui.edu>
2019-08-06 08:32:33 -0500

    Uploading templates and fixed images

--- pages/skaters.php
+++ pages/skaters.php
@@ -9,50 +9,111 @@
 
     <main>
         
+        <!-------------------------------------------------------------------->
+        
+        <div class="start"></div>
+        <h2 class="headingbar">Template box - Image Left</h2>    
+        <div class="row2">
+            <div class="columnleft2">
+                <img class="boximage" src="../images/logo.webp">
+                    aliqua. Ut enim ad minim veniam, quis nostrud.
+                    <p class="script">Lorem 
+                    ipsum dolor sit amet, consectetur adipiscing elit, sed 
+                    do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+            </div>
+
+            <div class="columnright2">
+                <p class="script">Here is a sample copy-paste information box.
+                </p>
+                <p class="script">Specs:<br>
+                30 pixel gap between info boxes.<br>
+                Header bar on each box.<br>
+                Left column: 25%<br>
+                Right column: 75%<br>
+                Left/Right/Center margins: 25 pixels<br>
+                Can alternate dark or white color themes.</p>
+            </div>
+        </div>
+        
+        <!-------------------------------------------------------------------->        
+
+        <h2 class="headingbar">Template box - Image Right</h2>    
         <div class="row">
             <div class="columnleft">
-                <img src="../images/logo.webp">
+                <p class="script2">Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                    et dolore magna Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                    consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit 
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                    occaecat cupidatat non proident, sunt in culpa.
             </div>
 
             <div class="columnright">
-                <p class="emphasistext">Flowery statement about how wonderful the staff is (especially the treasurer and her husband. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa.</p>
+                <img class="boximage" src="../images/logo.webp">
+                <p class="script2">Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                    et dolore magna aliqua. </p>
             </div>
         </div>
+        
+        <!-------------------------------------------------------------------->
+        
+        <h2 class="headingbar">Template box - Image Left</h2>    
+        <div class="row2">
+            <div class="columnleft2">
+                <img class="boximage" src="../images/logo.webp">
+                    aliqua. Ut enim ad minim veniam, quis nostrud.
+                    <p class="script">Lorem 
+                    ipsum dolor sit amet, consectetur adipiscing elit, sed 
+                    do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+            </div>
 
-        <h1 class="bar">Meet the Rebels:</h1>    
-        <img src="../images/portraits/p1.webp">
+            <div class="columnright2">
+                <p class="script">Flowery statement about how wonderful 
+                    the staff is (especially the treasurer and her husband. 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit 
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                    occaecat cupidatat non proident, sunt in culpa.</p>
+                <p class="script">Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                    consequat. Duis aute irure dolor in reprehenderit in 
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+            </div>
+        </div>
+        
+        <!-------------------------------------------------------------------->        
+
+        <h2 class="headingbar">Template box - Image Right</h2>    
         <div class="row">
             <div class="columnleft">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p><br>
+                <p class="script2">Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                    et dolore magna Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                    consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit 
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                    occaecat cupidatat non proident, sunt in culpa.
             </div>
 
             <div class="columnright">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa.</p><br>
-                <p>Lorem - ipsum dolor sit amet!<p><br>
-                <p>Ipsum - consectetur adipiscing elit!</p><br>
-                <p>Dolor - ullamco laboris nisi ut aliquip!</p><br>
-                <p>Exercitation - nisi ut aliquip ex ea commodo consequat!<br></p>
-
-
+                <img class="boximage" src="../images/logo.webp">
+                <p class="script2">Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor incididunt ut 
+                    labore et dolore magna aliqua.</p>
             </div>
-
-        </div>
-
-        <h1 class="bar">About Our Skaters</h1>    
-        <div class="row">
-
-            <div class="columnleft">
-                <p class="emphasistext">Honorable Mentions:</p><br>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
-                </p><br>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.</p>
-            </div>
-
-            <div class="columnright">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br><br>Excepteur sint occaecat cupidatat non proident, sunt in culpa. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa.</p>
-            </div>
-
-        </div>
+        </div>        
+        
+        <!-------------------------------------------------------------------->        
 
     </main>
 
