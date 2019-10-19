740405a31b7991581af4b51ab9f32e0eba3f2969
QBeck64 <kad16001@byui.edu>
2019-10-19 11:10:14 -0700

    Basic modal sekeleton added. Still needs css work done and table list created to display all items in SESSION. Adding items does add to SESSION , yippee! Only on store.php for now, once complete, changes will be added/modified to clothing.php

--- scripts/store.php
+++ scripts/store.php
@@ -40,7 +40,14 @@ while ($row = $storeItems->fetch(PDO::FETCH_ASSOC)) {
 	$qtySelect .= "</select>";
 
 		session_start();
-		//$itemArray = $_SESSION['item2']['name'];
+
+		//Check if items array already exists. If it does not, then instantiate
+		if (!isset($_SESSION['items'])) {
+			$_SESSION['items'] = array();
+		}
+
+		// Just for testing purposes
+		//$itemArray = $_SESSION['items']['2']['name'];
     
 
     /**********************************************************************
@@ -53,6 +60,28 @@ while ($row = $storeItems->fetch(PDO::FETCH_ASSOC)) {
             <img src='../images/store/$image
             ' alt='Image file not found'>
         </div>
+
+		// Cart modal *Still a Work in Progress
+		<div class='modal fade' id='cart' tabindex='-1' role='dialog'>
+			<div class='modal-dialog modal-lg' role='document'>
+				<div class='modal-content'>
+					<div class='modal-header'>
+						<h5 class='modal-title'>Shopping Cart</h5>
+						<button data-dismiss='modal' class='close' id='exitBtn" . $x . "'>x</button>
+					</div>
+					<div class='modal-body'>
+						<table class='show-cart table'>
+          
+						</table>
+						<div>Total price: $<span class='total-cart'></span></div>
+					</div>
+					<div class='modal-footer'>
+						<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
+						<button type='button' class='btn btn-primary'>Order now</button>
+					</div>
+				</div>
+			</div>
+		</div> 
 
         <div id='$modelID' class='modal'>
             <div class='modal-content'>
@@ -70,11 +99,13 @@ while ($row = $storeItems->fetch(PDO::FETCH_ASSOC)) {
 							$qtySelect
 						</div>
 						<div class='textblock' id='addItemsDiv'>
+						<input type='hidden' name='itemNum' value='" . $x . "'>
 							<input type='hidden' name='itemId' value='" . $item_id . "'>
 							<input type='hidden' name='itemName' value='" . $name . "'>
 							<input type='hidden' name='itemPrice' value='" . $price . "'>
 							<input type='hidden' name='availableQty' value='" . $quantity . "'>
 							<button type='submit' name='AddItem'>Add to cart</button>		
+							<p>$itemArray</p>
 						</div>
 					</form>
                 </div>
@@ -115,8 +146,9 @@ while ($row = $storeItems->fetch(PDO::FETCH_ASSOC)) {
 	// On Form Post set Session variables
 	// Set all variables
 	if (isset($_POST["AddItem"])) {
-	//$_SESSION['test'] = "hey";
-		$_SESSION['item2'] = array (
+	// PHP variable must have daat received from SESSION or POSt to be accepted as Parameters, dumb right?!
+	    $itemNum = $_POST["itemNum"];
+		$_SESSION['items'][$itemNum] = array (
 			'item_id' => $_POST['itemID'],
 			'name' => $_POST['itemName'],
 			'price' => $_POST['itemPrice'],
