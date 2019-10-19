b6340bff6bc844cad748a9c109377480cdb1b0f0
QBeck64 <kad16001@byui.edu>
2019-10-19 10:59:47 -0700

    modal

--- scripts/store.php
+++ scripts/store.php
@@ -57,6 +57,30 @@ while ($row = $storeItems->fetch(PDO::FETCH_ASSOC)) {
             ' alt='Image file not found'>
         </div>
 
+		// Cart modal
+		<div class='modal fade' id='cart' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true' style='display:block;'>
+  <div class='modal-dialog modal-lg' role='document'>
+    <div class='modal-content'>
+      <div class='modal-header'>
+        <h5 class='modal-title' id='exampleModalLabel'>Cart</h5>
+        <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>
+      <div class='modal-body'>
+        <table class='show-cart table'>
+          
+        </table>
+        <div>Total price: $<span class='total-cart'></span></div>
+      </div>
+      <div class='modal-footer'>
+        <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
+        <button type='button' class='btn btn-primary'>Order now</button>
+      </div>
+    </div>
+  </div>
+</div> 
+
         <div id='$modelID' class='modal'>
             <div class='modal-content'>
 			<button data-dismiss='modal' class='close' id='exitBtn" . $x . "'>x</button>
