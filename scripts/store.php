bbf13873b14c23ff2eb58c813470b78c5cdd1b4c
QBeck64 <kad16001@byui.edu>
2019-10-24 16:25:36 -0700

    stuff

--- scripts/store.php
+++ scripts/store.php
@@ -150,8 +150,9 @@ while ($row = $storeItems->fetch(PDO::FETCH_ASSOC)) {
     ";
 	// On Form Post set Session variables
 	// Set all variables
+	$addItem = 'AddItem' . $arrayIndex;
 	
-	if (isset($_POST['AddItem' . $arrayIndex . ''])) {
+	if (isset($_POST[$addItem])) {
 	// PHP variable must have daat received from SESSION or POSt to be accepted as Parameters, dumb right?!
 	    $itemNum = $_POST['itemNum'];
 		if (array_key_exists($itemNum, $_SESSION['items']))
