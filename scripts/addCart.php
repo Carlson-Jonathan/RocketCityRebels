4cbaf6d74a48c5de0803c58f4ed7a8ef1ab57f99
QBeck64 <kad16001@byui.edu>
2019-10-25 11:09:04 -0700

    stuff

--- scripts/addCart.php
+++ scripts/addCart.php
@@ -13,6 +13,23 @@ session_start();
 echo '<script>window.location.href = "../pages/store.php";</script>';
 
 // Security
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
+// Set all variables
+	// PHP variable must have daat received from SESSION or POSt to be accepted as Parameters, dumb right?!
+	    $itemNum = $_POST["itemNum"];
+		$newArray = array (
+			'item_id' => $_POST['itemID'],
+			'name' => $_POST['itemName'],
+			'price' => $_POST['itemPrice'],
+			'qty' => $_POST["availableQty"],
+			'selectQty' => $_POST['selectQty'],
+		);
+		$_SESSION['items'][$itemNum] = $newArray);
 header("Location: ../pages/store.php");
 ?>
