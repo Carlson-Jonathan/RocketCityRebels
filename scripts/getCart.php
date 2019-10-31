e3276bdd9487bd38b172edfa94e4aad80954375b
QBeck64 <kad16001@byui.edu>
2019-10-31 14:15:44 -0700

    test

--- scripts/getCart.php
+++ scripts/getCart.php
@@ -23,7 +23,7 @@ $clothingArray = '';
 end($_SESSION['items']);
 $key = key($_SESSION['items']);
 
-while ($itemCount < $key) {
+while ($itemCount <= $key) {
 	if (isset($_SESSION['items'][$itemCount]) && !empty($_SESSION['items'][$itemCount])) {
 		$itemArray .= "<tr><form method='POST' action='../scripts/removeCartItem.php?itemArray_id=" . $itemCount . "'>
 						<td><input type='submit' value='X'></td>
